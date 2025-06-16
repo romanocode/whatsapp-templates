bb48409374dbaa48dfeaeaf256e92831c2cb3bfc
romanocode <carlos1996romano@gmail.com>
2025-06-15 21:08:37 -0500

    Maquetacion completa

---
+++ js/models/models.js
@@ -0,0 +1,9 @@
+// creamos la clase template
+
+class Template{
+    constructor(titulo,mensaje,hashtag){
+        this.titulo = titulo ;
+        this.mensaje = mensaje;
+        this.hashtag = hashtag
+    }
+}
